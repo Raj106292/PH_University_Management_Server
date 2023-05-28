e1a15080a22a0f83516096e2ddd12ccee7b0a71c
Raj106292 <rajdas.106292@gmail.com>
2023-05-29 04:06:10 +0600

    basic server created

---
+++ .src/server.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from '../config/index';
+
+
+async function bootstrap() {
+    try {
+        await mongoose.connect(config.database_url as string);
+        console.log("😍 Database connected successfully");
+
+        app.listen(config.port, () => {
+            console.log(`Server is running on port ${config.port}`);
+        });
+    } catch (error) {
+        console.log("Failed to connect database", error);
+    }
+}
+
+bootstrap();
