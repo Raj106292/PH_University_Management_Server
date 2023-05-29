5db60b95c00f40c8c6d9e1318995383c7a519260
Raj106292 <rajdas.106292@gmail.com>
2023-05-30 00:42:14 +0600

    lint and format checked

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,9 @@
-import dotenv from 'dotenv';
-import path from 'path';
+import dotenv from 'dotenv'
+import path from 'path'
 
-dotenv.config({ path: path.join(process.cwd(), '.env') }); // cwd => current working directory
+dotenv.config({ path: path.join(process.cwd(), '.env') }) // cwd => current working directory
 
 export default {
-    port: process.env.PORT,
-    database_url: process.env.DATABASE_URL
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
 }
