dac518c5a869477600ea46ac3d1274bbe371439b
Raj106292 <rajdas.106292@gmail.com>
2023-06-02 02:38:32 +0600

    improve lint setup

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import userRouter from './app/modules/users/users.route'
 
 const app: Application = express()
 
@@ -7,7 +8,10 @@ app.use(cors()) // cors
 app.use(express.json()) // parser
 app.use(express.urlencoded({ extended: true })) // encoded data
 
-app.get('/', (req: Request, res: Response) => {
+// Application routes
+app.use('/api/v1/users', userRouter)
+
+app.get('/', async (req: Request, res: Response) => {
   res.send('Server is ready to work')
 })
 
--- src/app/modules/users/users.service.ts
+++ src/app/modules/users/users.service.ts
@@ -1,9 +1,18 @@
 import { IUser } from './users.interface'
 import { User } from './users.model'
+import config from '../../../config/index' // default export
+import { generateUserId } from './users.utilies'
 
 const createUser = async (user: IUser): Promise<IUser | null> => {
   // auto generated incremental id
+  const id = await generateUserId()
+  user.id = id
+
   // default password
+  if (!user.password) {
+    user.password = config.default_user_pass as string
+  }
+
   const createdUser = await User.create(user)
   if (!createUser) {
     throw new Error('Failed to create user')
