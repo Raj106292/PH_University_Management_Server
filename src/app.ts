628d43fcb5a3a18c811ef8fe5c4ab7c2d34da0e9
Raj106292 <rajdas.106292@gmail.com>
2023-06-05 10:17:20 +0600

    code optimized first part

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from 'cors'
 import express, { Application, Request, Response } from 'express'
-import userRouter from './app/modules/users/users.route'
+import globalErrorHandler from './app/middleware/globalErrorHandler'
+import { UserRoute } from './app/modules/user/user.route'
 
 const app: Application = express()
 
@@ -9,36 +10,13 @@ app.use(express.json()) // parser
 app.use(express.urlencoded({ extended: true })) // encoded data
 
 // Application routes
-app.use('/api/v1/users', userRouter)
-
-// create my own format for the error
-// class APIError extends Error {
-//   statusCode: number
-
-//   constructor(statusCode: number, message: string | undefined, stack = '') {
-//     super(message)
-//     this.statusCode = statusCode
-//     if (stack) {
-//       this.stack = stack
-//     } else {
-//       Error.captureStackTrace(this, this.constructor)
-//     }
-//   }
-// }
+app.use('/api/v1/users', UserRoute)
 
 app.get('/', async (req: Request, res: Response) => {
   res.send('Server is ready to work')
-  // throw new Error("No man! It's an error")
-  // next("No man! It's an error");
 })
 
-// global error handler
-// app.use((err, req: Request, res: Response, next: NextFunction) => {
-//   if (err instanceof Error) {
-//     res.status(400).json({})
-//   } else {
-//     res.status(500).json({ error: 'Something went wrong' })
-//   }
-// })
+//global error handler
+app.use(globalErrorHandler)
 
 export default app
